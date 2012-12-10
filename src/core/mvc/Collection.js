133fc77e415ce125aa4942a0b7087cffba33f7a0
Sebastian Werner <info@sebastian-werner.net>
2012-12-10 10:40:31 +0100

    More MVC support

--- src/core/mvc/Collection.js
+++ src/core/mvc/Collection.js
@@ -14,6 +14,7 @@ core.Class("core.mvc.Collection",
   construct: function(models) 
   {
     this.__models = [];
+    this.__length = 0;
 
     if (models != null) {
       this.append(models);
@@ -42,12 +43,12 @@ core.Class("core.mvc.Collection",
       var db = this.__models;
 
       var addEvent = core.mvc.event.AddModel.obtain(null);
-      addEvent.target = this;
       
       for (var i=0, l=models.length, model; i<l; i++) 
       {
         model = models[i];
         db.push(model);
+        this.length++;
 
         addEvent.model = model;
         this.dispatchEvent(addEvent);
@@ -64,26 +65,99 @@ core.Class("core.mvc.Collection",
       var db = this.__models;
 
       var removeEvent = core.mvc.event.RemoveModel.obtain(null);
-      removeEvent.target = this;
 
-      for (var i=0, l=models.length, model; i<l; i++) 
+      for (var i=db.length, model; i>=0; i--) 
       {
         model = models[i];
-        db.push(model);
 
         removeEvent.model = model;
         this.dispatchEvent(removeEvent);
+
+        // Pop out last model
+        db.length--;
+
+        // Update internal length, too
+        this.length--;
       }
 
       core.mvc.event.RemoveModel.release(removeEvent);
+    },
 
-      this.__models.length = 0;
+
+    at : function(index) {
+      return this.__models[index] || null;
     },
 
     pop : function() 
     {
+      var removedModel = this.__models.pop();
+      if (!removedModel) {
+        return;
+      }
+      
+      var removeEvent = core.mvc.event.RemoveModel.obtain(removedModel);
+      this.dispatchEvent(removeEvent);
+      core.mvc.event.RemoveModel.release(removeEvent);
+    },
+
+    push : function() {
 
     },
+
+    shift : function() 
+    {
+      var removedModel = this.__models.shift();
+      if (!removedModel) {
+        return;
+      }
+
+      var removeEvent = core.mvc.event.RemoveModel.obtain(removedModel);
+      this.dispatchEvent(removeEvent);
+      core.mvc.event.RemoveModel.release(removeEvent);
+
+      return removedModel;
+    },
+
+    unshift: function() {
+
+    },
+
+    filter : function() {
+
+    },
+
+    map : function(func) {
+      return this.__models.map(func);
+    },
+
+
+    /**
+     * Pluck an attribute from each model in the collection. 
+     * Equivalent to calling `map`, and returning a single attribute from the iterator.
+     */
+    pluck : function(property) 
+    {
+      return this.__models.map(function() {
+        return this.get(property);
+      });
+    },
+
+
+    /**
+     * {core.mvc.Model} Returns the model with the given ID.
+     */ 
+    get : function(id) 
+    {
+      for (var i=0, l=models.length; i<l; i++) 
+      {
+        if (models[i].getId() == id) {
+          return models[i];
+        }
+      }
+
+      return null;
+    },
+
 
     /**
      * {String} Returns the JSON representation of the stored data.

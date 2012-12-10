6585b2f32680c8898f93f68567ce3699e135ed15
Sebastian Werner <info@sebastian-werner.net>
2012-12-10 10:18:15 +0100

    Added more MVC features

--- src/core/mvc/Collection.js
+++ src/core/mvc/Collection.js
@@ -1,9 +1,98 @@
 /**
- *
+ * Collections are ordered sets of models. You can bind individual "change" events 
+ * to be notified when any model in the collection has been modified, 
+ * listen for "add" and "remove" events, fetch the collection from the 
+ * server, etc.
  */
 core.Class("core.mvc.Collection", 
 {
-  construct: function() {
-  
-  }  
+  include: [core.property.MGeneric, core.event.MEvent],
+
+  /**
+   * Prefill the collection with @models {core.mvc.Model[]}.
+   */
+  construct: function(models) 
+  {
+    this.__models = [];
+
+    if (models != null) {
+      this.append(models);
+    }
+  },
+
+  properties : 
+  {
+    /**
+     * Model to use for creating new entries.
+     */
+    model : 
+    {
+      check: core.mvc.Model,
+      nullable : true
+    }
+  },
+
+  members :
+  {
+    /**
+     * Imports an array of @models {core.mvc.Model[]} into the collection.
+     */
+    append : function(models) 
+    {
+      var db = this.__models;
+
+      var addEvent = core.mvc.event.AddModel.obtain(null);
+      addEvent.target = this;
+      
+      for (var i=0, l=models.length, model; i<l; i++) 
+      {
+        model = models[i];
+        db.push(model);
+
+        addEvent.model = model;
+        this.dispatchEvent(addEvent);
+      }
+
+      core.mvc.event.AddModel.release(addEvent);
+    },
+
+    /**
+     * Clears the collection so that all models are removed from it.
+     */
+    clear : function() 
+    {
+      var db = this.__models;
+
+      var removeEvent = core.mvc.event.RemoveModel.obtain(null);
+      removeEvent.target = this;
+
+      for (var i=0, l=models.length, model; i<l; i++) 
+      {
+        model = models[i];
+        db.push(model);
+
+        removeEvent.model = model;
+        this.dispatchEvent(removeEvent);
+      }
+
+      core.mvc.event.RemoveModel.release(removeEvent);
+
+      this.__models.length = 0;
+    },
+
+    pop : function() 
+    {
+
+    },
+
+    /**
+     * {String} Returns the JSON representation of the stored data.
+     */
+    toJSON : function() 
+    {
+      return "[" + this.__models.map(function(model) {
+        return model.toJSON();
+      }).join(",") + "]";
+    }
+  }
 });
--- src/core/mvc/Model.js
+++ src/core/mvc/Model.js
@@ -2,30 +2,40 @@
  * Models are the heart of any JavaScript application, 
  * containing the interactive data as well as a large 
  * part of the logic surrounding it: conversions, validations, 
- * computed properties, and access control. You extend Backbone.Model 
- * with your domain-specific methods, and Model provides a basic 
+ * computed properties, and access control. You extend `core.mvc.Model`
+ * with your domain-specific methods, and `Model` provides a basic 
  * set of functionality for managing changes.
  */
 core.Class("core.mvc.Model", 
 {
   include: [core.property.MGeneric, core.event.MEvent],
 
+  /**
+   * Initial data structure is imported from @values {Map}.
+   */
   construct: function(values) 
   {
-    console.debug("Model Values: ", values);
+    if (jasy.Env.isSet("debug")) 
+    {
+      if (values != null) {
+        core.Assert.isType(values, "Map", "Invalid values to import!");
+      }
+    }
+
     if (values != null) {
       this.set(values);
     }
-
   },
 
   members: 
   {
-    toJson : function() 
+    /**
+     * {String} Exports all property data into a JSON structure.
+     */
+    toJSON : function() 
     {
       var properties = Object.keys(core.Class.getProperties(this.constructor));
       return JSON.stringify(this.get(properties));
     }
-
   }
 });

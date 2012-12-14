ab0703b6eaa379cb87f43dbb891e813931a0a4cc
Sebastian Werner <info@sebastian-werner.net>
2012-12-14 10:40:42 +0100

    Fixed global reference

--- test/source/class/Namespace.js
+++ test/source/class/Namespace.js
@@ -1,17 +1,20 @@
-module("Core :: Namespaces", {
-  teardown : function() {
-    delete global.foo;
-    delete global.abc;
-    delete global.a;
-  }
-});
+(function(global) 
+{
+  module("Core :: Namespaces", {
+    teardown : function() {
+      delete global.foo;
+      delete global.abc;
+      delete global.a;
+    }
+  });
 
-test("Creating global", function() {
-  core.Main.declareNamespace("foo", 3);
-  equal(global.foo, 3);
-});
+  test("Creating global", function() {
+    core.Main.declareNamespace("foo", 3);
+    equal(global.foo, 3);
+  });
 
-test("Creating namespace", function() {
-  core.Main.declareNamespace("abc.def", 5);
-  equal(global.abc.def, 5);
-});
+  test("Creating namespace", function() {
+    core.Main.declareNamespace("abc.def", 5);
+    equal(global.abc.def, 5);
+  });
+})(this);

ae25398a020197b63775ecb62bd04f1f7c8a9747
Sebastian Werner <info@sebastian-werner.net>
2012-12-14 14:25:19 +0100

    Cleanup of help strings + added basic phantom task

--- test/jasyscript.py
+++ test/jasyscript.py
@@ -2,7 +2,7 @@
 session.setField("debug", True)
 
 
-@task("Source")
+@task
 def source():
     # Initialize shared objects
     assetManager = AssetManager(session).addSourceProfile()
@@ -22,7 +22,7 @@ def source():
         outputManager.storeLoader(classes, "$prefix/script/test-$permutation.js")
 
 
-@task("Build")
+@task
 def build():
     # Initialize shared objects
     assetManager = AssetManager(session).addBuildProfile()
@@ -48,13 +48,50 @@ def build():
         outputManager.storeCompressed(classes, "$prefix/script/test-$permutation.js")
     
     
-@task("Clear cache")
+@task
+def phantom():
+    # Initialize shared objects
+    assetManager = AssetManager(session).addSourceProfile()
+    outputManager = OutputManager(session, assetManager, compressionLevel=0, formattingLevel=1)
+    fileManager = FileManager(session)
+    
+    # Store kernel script
+    outputManager.storeKernel("$prefix/script/kernel.js", debug=True)
+    
+    # Copy files from source
+    fileManager.updateFile("source/phantom.js", "$prefix/phantom.js")
+    fileManager.updateFile("source/phantom.html", "$prefix/phantom.html")
+
+    # Process every possible permutation
+    for permutation in session.permutate():
+
+        # Resolving dependencies
+        classes = Resolver(session).addClassName("test.Main").getSortedClasses()
+        
+        # Writing source loader
+        outputManager.storeLoader(classes, "$prefix/script/test-$permutation.js", "phantomQunit();", urlPrefix="../source")
+
+    # Execute test
+    Console.info("Running QUnit...")
+    import jasy.core.Util
+    result = None
+    try:
+        result = jasy.core.Util.executeCommand("phantomjs phantom.js", "Tests did not ececute successfully", "phantom")
+    except Exception:
+        pass
+
+    Console.info("QUnit Finished")
+    print(result)
+
+
+
+@task
 def clean():
     session.clean()
     Repository.clean()
 
 
-@task("Clear cache and repository")
+@task
 def distclean():
     session.clean()
     Repository.distclean()

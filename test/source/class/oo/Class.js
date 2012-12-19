c5fbd4ef5ddf89ff3d3dcd31caba20e3c4256f83
Sebastian Werner <info@sebastian-werner.net>
2012-12-19 18:14:19 +0100

    Fixed HTML test

--- test/source/class/oo/Class.js
+++ test/source/class/oo/Class.js
@@ -662,6 +662,7 @@ suite.test("Inheriting Properties", function()
     
     construct : function() 
     {
+      // Fake DOM element
       this.__labelElement = typeof document != "undefined" ? document.createElement("label") : {style:{}};
       
       properties.Text.call(this, this.__labelElement);

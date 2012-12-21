b5f59a793caa9103a901b858ac50c5b06a97341c
Sebastian Werner <info@sebastian-werner.net>
2012-12-21 09:20:03 +0100

    Cleanup

--- test/source/node.js
+++ test/source/node.js
@@ -3,8 +3,4 @@
 var fs = require("fs");
 
 eval(fs.readFileSync("script/kernel.js", "utf-8"));
-
-console.debug("Is Browser: " + jasy.Env.isSet("runtime", "browser"))
-
 eval(fs.readFileSync("script/test-" + jasy.Env.CHECKSUM + ".js", "utf-8"));
-

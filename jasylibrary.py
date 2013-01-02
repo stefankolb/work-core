d1a4977b86f0a3c0dac46931c6d563d79f9edcb1
Sebastian Werner <info@sebastian-werner.net>
2013-01-02 14:46:29 +0100

    Moved out api task into library for external use.

---
+++ jasylibrary.py
@@ -0,0 +1,48 @@
+from jasy.core.OutputManager import OutputManager
+from jasy.core.FileManager import FileManager
+from jasy.asset.Manager import AssetManager
+from jasy.js.Resolver import Resolver
+from jasy.js.api.Writer import ApiWriter
+
+@share
+def api(theme="original"):
+  """Generates the API viewer for the current project"""
+
+  import json
+
+  # Configure fields
+  session.setField("debug", False)
+  session.setField("apibrowser.theme", theme)
+  session.permutateField("es5")
+
+  # Initialize shared objects
+  assetManager = AssetManager(session).addBuildProfile()
+  outputManager = OutputManager(session, assetManager, compressionLevel=2)
+  fileManager = FileManager(session)
+
+  # Deploy assets
+  outputManager.deployAssets(["core.apibrowser.Browser"])
+
+  # Write kernel script
+  outputManager.storeKernel("$prefix/script/kernel.js", debug=True)
+
+  # Copy files from source
+  fileManager.updateFile("source/apibrowser.html", "$prefix/index.html")
+  
+  # Rewrite template as jsonp
+  for tmpl in ["main", "error", "entry", "type", "params", "info", "origin", "tags"]:
+      jsonTemplate = json.dumps({ "template" : open("source/tmpl/apibrowser/%s.mustache" % tmpl).read() })
+      fileManager.writeFile("$prefix/tmpl/%s.js" % tmpl, "apiload(%s, '%s.mustache')" % (jsonTemplate, tmpl))
+      
+  # Process every possible permutation
+  for permutation in session.permutate():
+      
+      # Resolving dependencies
+      resolver = Resolver(session).addClassName("core.apibrowser.Browser")
+
+      # Compressing classes
+      outputManager.storeCompressed(resolver.getSortedClasses(), "$prefix/script/apibrowser-$permutation.js", "new core.apibrowser.Browser;")
+
+  # Write data
+  ApiWriter(session).write("$prefix/data")
+

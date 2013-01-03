b97233ce5eb66230ecb933bf1dc5867e12537374
Sebastian Werner <info@sebastian-werner.net>
2013-01-03 10:42:01 +0100

    Indent fixes

--- jasylibrary.py
+++ jasylibrary.py
@@ -71,44 +71,44 @@ def executeCommand(args, failMessage=None, path=None, wrapOutput=True):
 
 @share
 def api():
-  """Generates the API viewer for the current project"""
+    """Generates the API viewer for the current project"""
 
-  sourceFolder = session.getProjectByName("core").getPath() + "/source/"
+    sourceFolder = session.getProjectByName("core").getPath() + "/source/"
 
-  # Configure fields
-  session.setField("debug", False)
-  session.permutateField("es5")
+    # Configure fields
+    session.setField("debug", False)
+    session.permutateField("es5")
 
-  # Initialize shared objects
-  assetManager = AssetManager(session).addBuildProfile()
-  outputManager = OutputManager(session, assetManager, compressionLevel=2)
-  fileManager = FileManager(session)
+    # Initialize shared objects
+    assetManager = AssetManager(session).addBuildProfile()
+    outputManager = OutputManager(session, assetManager, compressionLevel=2)
+    fileManager = FileManager(session)
 
-  # Deploy assets
-  outputManager.deployAssets(["core.apibrowser.Browser"])
+    # Deploy assets
+    outputManager.deployAssets(["core.apibrowser.Browser"])
 
-  # Write kernel script
-  outputManager.storeKernel("$prefix/script/kernel.js", debug=True)
+    # Write kernel script
+    outputManager.storeKernel("$prefix/script/kernel.js", debug=True)
 
-  # Copy files from source
-  fileManager.updateFile(sourceFolder + "/apibrowser.html", "$prefix/index.html")
-  
-  # Rewrite template as jsonp
-  for tmpl in ["main", "error", "entry", "type", "params", "info", "origin", "tags"]:
-      jsonTemplate = json.dumps({ "template" : open(sourceFolder + "/tmpl/apibrowser/%s.mustache" % tmpl).read() })
-      fileManager.writeFile("$prefix/tmpl/%s.js" % tmpl, "apiload(%s, '%s.mustache')" % (jsonTemplate, tmpl))
-      
-  # Process every possible permutation
-  for permutation in session.permutate():
-      
-      # Resolving dependencies
-      resolver = Resolver(session).addClassName("core.apibrowser.Browser")
+    # Copy files from source
+    fileManager.updateFile(sourceFolder + "/apibrowser.html", "$prefix/index.html")
+    
+    # Rewrite template as jsonp
+    for tmpl in ["main", "error", "entry", "type", "params", "info", "origin", "tags"]:
+        jsonTemplate = json.dumps({ "template" : open(sourceFolder + "/tmpl/apibrowser/%s.mustache" % tmpl).read() })
+        fileManager.writeFile("$prefix/tmpl/%s.js" % tmpl, "apiload(%s, '%s.mustache')" % (jsonTemplate, tmpl))
+        
+    # Process every possible permutation
+    for permutation in session.permutate():
+        
+        # Resolving dependencies
+        resolver = Resolver(session).addClassName("core.apibrowser.Browser")
 
-      # Compressing classes
-      outputManager.storeCompressed(resolver.getSortedClasses(), "$prefix/script/apibrowser-$permutation.js", "new core.apibrowser.Browser;")
+        # Compressing classes
+        outputManager.storeCompressed(resolver.getSortedClasses(), "$prefix/script/apibrowser-$permutation.js", "new core.apibrowser.Browser;")
 
-  # Write API data
-  ApiWriter(session).write("$prefix/data")
+    # Write API data
+    ApiWriter(session).write("$prefix/data")
 
 
 @share

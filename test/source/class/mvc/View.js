cd299390c97a5f1ce4afca93c61fb07678131801
Sebastian Werner <info@sebastian-werner.net>
2013-01-04 16:26:20 +0100

    Worked on view test

--- test/source/class/mvc/View.js
+++ test/source/class/mvc/View.js
@@ -6,5 +6,62 @@ suite.test("Empty", function()
 
   this.instance(empty, core.mvc.View);
 
+  this.not(empty.getModel());
+  this.not(empty.getTemplate());
+});
+
+suite.test("Constructor", function() 
+{
+  core.Class("TestView",
+  {
+    include : [core.mvc.View],
+
+    construct : function(properties, renderCallBack) {
+      this.renderCallBack = renderCallBack;
+      core.mvc.View.call(this, properties);
+    },
+
+    members : 
+    {
+      render : function() {
+        this.renderCallBack();
+      }
+    }
+  });
+
+  core.Class("TestModel",
+  {
+    include : [core.mvc.Model],
+
+    construct : function(values) {
+      core.mvc.Model.call(this, values);
+    },
+
+    properties : 
+    {
+      name : {
+        type : "String",
+      }
+    }
+  });  
+
+  var model = new TestModel;
+  var template = core.template.Compiler.compile("Hello {{name}}");
+
+  var renderCount = 0;
+  var test = new TestView({
+    model : model,
+    template : template
+  }, function() {
+    renderCount++;
+  });
+
+  this.instance(test, TestView);
+  this.identical(test.getModel(), model);
+  this.identical(test.getTemplate(), template);
+  
+  // Two expected calls - one for model change, one for template change
+  this.identical(renderCount, 2);
+
 });
 

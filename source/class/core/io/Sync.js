8ce60e56c3762f6d7e8b81704ca981b55d37c0c3
Sebastian Werner <info@sebastian-werner.net>
2013-01-14 20:00:58 +0100

    Added sync loader

---
+++ source/class/core/io/Sync.js
@@ -0,0 +1,19 @@
+(function(doc) 
+{
+  var doc = document;
+
+  var replaceFields = function(inputString) {
+    return inputString.replace("%checksum%", jasy.Env.CHECKSUM);
+  };
+
+  core.Module("core.io.Sync",
+  {
+    loadStyleSheet : function(assetId) {
+      doc.write("<link rel='stylesheet' href='" + jasy.Asset.toUri(assetId) + "'/>");
+    },
+
+    loadScript : function(scriptId) {
+      doc.write("<script src='" + replaceFields(scriptId) + "'></script>")
+    }
+  });
+})();

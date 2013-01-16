7c88cf91e7b20eda08cd46b18e9761dbdcfee03f
Sebastian Werner <info@sebastian-werner.net>
2013-01-16 16:47:50 +0100

    Added parse() to model interface to transform data from server representation to client side properties. Improved docs for clientId.

--- source/class/core/mvc/model/IModel.js
+++ source/class/core/mvc/model/IModel.js
@@ -13,7 +13,7 @@ core.Interface("core.mvc.model.IModel",
   events :
   {
     /** 
-     * Fired whenever the model do change 
+     * Fired whenever the model did change 
      */
     change : core.event.Simple
   },
@@ -31,14 +31,25 @@ core.Interface("core.mvc.model.IModel",
     setId : function(identifier) {},
 
     /** 
-     * {String} Returns a unique client side identifier for this model
-     * which is automatically set on instantiation.
+     * {String} A special property of models, the client id is a unique identifier 
+     * automatically assigned to all models when they're first created. Client ids 
+     * are handy when the model has not yet been saved to the server, and does not 
+     * yet have its eventual true id, but already needs to be visible in the UI.
      */
     getClientId : function() {},    
 
     /**
      * {String} Exports all model data into a JSON structure.
      */    
-    toJSON : function() {}
+    toJSON : function() {},
+
+    /**
+     * The function is passed the raw response object, and should return the properties
+     * to be set on the model. The default implementation is a no-op, simply passing 
+     * through the response (which should be JSON and where the keys match the name
+     * of the model properties). Override this if you need to work with a preexisting API, 
+     * or better namespace your responses.
+     */
+    parse : function() {}
   }
 });

3c84b537643d297ae689e6a6ec9c81a63c24d681
Sebastian Werner <info@sebastian-werner.net>
2013-01-17 13:25:37 +0100

    Added Presenter

---
+++ source/class/core/mvc/presenter/Abstract.js
@@ -0,0 +1,16 @@
+core.Class("core.mvc.presenter.Abstract",
+{
+  include: [core.event.MEventTarget, core.util.MLogging],
+
+  construct : function() 
+  {
+    
+  },
+
+  members :
+  {
+
+
+
+  }
+});

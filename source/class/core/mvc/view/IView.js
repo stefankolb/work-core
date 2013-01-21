c0e9587e79497cc9386ef8fbbc06ef83cf998ec3
Sebastian Werner <info@sebastian-werner.net>
2013-01-21 11:23:13 +0100

    Added view interface. Moved around code blocks between view classes.

---
+++ source/class/core/mvc/view/IView.js
@@ -0,0 +1,39 @@
+core.Interface("core.mvc.view.IView",
+{
+  /**
+   * @presenter {core.mvc.presenter.Abstract} Presenter instance to connect to
+   */  
+  construct: function(presenter) {},
+  
+  events :
+  {
+    /** Fired after the view has been shown */
+    "show" : core.event.Simple,
+
+    /** Fired after the view has been hidden */
+    "hide" : core.event.Simple
+  },
+
+  members :
+  {
+    /**
+     * {core.mvc.presenter.Abstract} Returns the attached presenter instance
+     */
+    getPresenter : function() {},
+
+    /**
+     * Renders the view using data from the attached presenter.
+     */
+    render : function() {},
+
+    /**
+     * Hides the view.
+     */
+    hide : function() {},
+
+    /**
+     * Shows the view.
+     */
+    show : function() {}
+  }
+});

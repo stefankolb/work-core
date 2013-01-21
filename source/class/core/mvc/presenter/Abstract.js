125bb5bda49ca5b839bfccb106e78185c48a7b46
Sebastian Werner <info@sebastian-werner.net>
2013-01-21 13:25:05 +0100

    Minor fixes + copyright updates

--- source/class/core/mvc/presenter/Abstract.js
+++ source/class/core/mvc/presenter/Abstract.js
@@ -1,3 +1,10 @@
+/*
+==================================================================================================
+  Core - JavaScript Foundation
+  Copyright 2013 Sebastian Werner
+==================================================================================================
+*/
+
 core.Class("core.mvc.presenter.Abstract",
 {
   include: [core.property.MGeneric, core.event.MEventTarget, core.util.MLogging],

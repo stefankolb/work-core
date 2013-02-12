effe9c2e7b3ef7529d9f098dd06df0a17a6c85db
Sebastian Werner <info@sebastian-werner.net>
2013-02-12 12:29:22 +0100

    Added support for parent presenter

--- source/class/core/mvc/presenter/Abstract.js
+++ source/class/core/mvc/presenter/Abstract.js
@@ -9,14 +9,24 @@ core.Class("core.mvc.presenter.Abstract",
 {
   include: [core.property.MGeneric, core.event.MEventTarget, core.util.MLogging],
 
-  construct : function() 
+  /**
+   * @parent {core.mvc.presenter.Abstract} Parent presenter to attach to this presenter
+   */
+  construct : function(parent) 
   {
-    
+
+    // Keep reference to parent presenter
+    this.__parent = parent;
   },
 
   members :
   {
-
+    /**
+     * {core.mvc.presenter.Abstract} Returns the parent presenter.
+     */
+    getParent : function() {
+      return this.__presenter;
+    }
 
 
   }

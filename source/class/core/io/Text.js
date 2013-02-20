562fb221fd1a488ee1a51234c6c627284b890a43
Sebastian Fastner <mail@sebastianfastner.de>
2013-02-20 21:56:52 +0100

    Refactor Text loader to use new XHR module

--- source/class/core/io/Text.js
+++ source/class/core/io/Text.js
@@ -12,14 +12,6 @@
  */
 (function(global) 
 {
-	var empty = new Function;
-	
-	// Detect native support
-	var XHR = global.XMLHttpRequest;
-	
-	// Dynamic URI can be shared because we do not support reloading files
-	var dynamicExtension = "?r=" + Date.now();
-	
 	/**
 	 * Generic loader for any text content using XMLHTTPRequests.
 	 */
@@ -35,69 +27,22 @@
 		 */
 		load : function(uri, callback, context, nocache, timeout) 
 		{
-			if (!context) {
-				context = global;
+            if (!context) {
+    			context = global;
 			}
-			
-			var timeoutHandle = null;
-			var request = XHR ? new XHR : new ActiveXObject("Microsoft.XMLHTTP");
-			
-			// Open request, we always use async GET here
-			request.open("GET", uri + (nocache ? dynamicExtension : ""), true);
-			
-			// Attach event listener
-			request.onreadystatechange = function(e) 
-			{
-				// Headers received... data following, now configuring the timeout
-				// As we don't send any data it's okay to start the timeout at this state.
-				if (request.readyState == 2 && timeout !== 0 && !timeoutHandle)
-				{
-					timeoutHandle = setTimeout(function() {
-						request.onreadystatechange = empty;
-						request.abort();
-						callback.call(context, uri, true);
-					}, timeout || 10000);
-				}
-				
-				if (request.readyState == 4) 
-				{
-					request.onreadystatechange = empty;
-					clearTimeout(timeoutHandle);
-
-					// Fixes for IE memory leaks
-					if (jasy.Env.isSet("engine", "trident") && global.detachEvent) {
-						global.detachEvent("onunload", onUnload);
-					}
-					
-					// Finally call the user defined callback (succeed with data)
-					var status = request.status;
-					callback.call(context, uri, !(status >= 200 && status < 300 || status == 304 || status == 1223), { 
-						text : request.responseText || ""
-					});
-				}
-			};
-			
-			// Fixes for IE memory leaks
-			if (jasy.Env.isSet("engine", "trident") && global.attachEvent) 
-			{
-				var onUnload = function() 
-				{
-					global.detachEvent("onunload", onUnload);
-					request.onreadystatechange = empty;
-					clearTimeout(timeoutHandle);
-					
-					// Internet Explorer will keep connections alive if we don't abort on unload
-					request.abort();
-					
-					// Finally call the user defined callback (failed)
-					callback.call(context, uri, true);
-				};
-				
-				global.attachEvent("onunload", onUnload);
-			}
-			
-			// Send request
-			request.send();
+            
+			core.io.Xhr.request("GET", uri, null, function(request, failed) 
+            {
+                if (failed) {
+                    callback.call(context, uri, true);
+                } else {
+        		    // Finally call the user defined callback (succeed with data)
+    				var status = request.status;
+    				callback.call(context, uri, !(status >= 200 && status < 300 || status == 304 || status == 1223), { 
+    					text : request.responseText || ""
+    				});
+                }
+			}, nocache, timeout);
 		}
 	});
 })(core.Main.getGlobal());

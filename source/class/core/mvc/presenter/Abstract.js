d0500a8ae2be2973f32f2e3443160cb07962d69a
Sebastian Werner <info@sebastian-werner.net>
2013-02-27 17:24:20 +0100

    Added view managment

--- source/class/core/mvc/presenter/Abstract.js
+++ source/class/core/mvc/presenter/Abstract.js
@@ -19,8 +19,9 @@ core.Class("core.mvc.presenter.Abstract",
       this.__parent = parent;  
     }
 
-    // Child presenters
+    // Child presenters and views
     this.__children = {};
+    this.__views = {};
   },
 
   members :
@@ -61,14 +62,112 @@ core.Class("core.mvc.presenter.Abstract",
 
     /*
     ======================================================
+      VIEW MANAGMENT
+    ======================================================
+    */
+
+    getView : function(name) {
+      return this.__views[name];
+    },
+
+    addView : function(name, view) 
+    {
+      var db = this.__views;
+
+      if (jasy.Env.isSet("debug")) 
+      {
+        if (name in db) {
+          throw new Error("View name " + name + " is already in use!");  
+        }
+        
+        if (!core.Main.isTypeOf(view, "Object")) {
+          throw new Error("Invalid view instance: " + view);
+        }
+      }
+
+      db[name] = view;
+      return view;
+    },
+
+
+    /**
+     * {Boolean} Removes the given child by its @name {String}. Returns whether it succeeded.
+     */
+    removeViewByName : function(name) {
+      return delete this.__views[name];
+    },
+
+
+    /**
+     * {Boolean} Removes the given @child {Object}. Returns whether it succeeded.
+     */
+    removeView : function(child) 
+    {
+      var db = this.__views;
+      for (var name in db) 
+      {
+        if (db[name] === child) {
+          return delete db[name];
+        }
+      }
+
+      return false;
+    },
+
+
+    /**
+     * {Object} Creates and registers a view under the given @name {String}
+     * using the given view @construct {Class}. Supports optional arguments
+     * using @varargs {var...} which are passed to the constructor. Returns the
+     * view instance which was created.
+     */
+    createView : function(name, construct, varargs) 
+    {
+      var args = arguments;
+
+      if (args.length > 2)
+      {
+        if (args.length == 3) {
+          var view = new construct(this, args[2]);
+        } else if (args.length == 4) {
+          var view = new construct(this, args[2], args[3]);
+        } else if (args.length == 5) {
+          var view = new construct(this, args[2], args[3], args[4]);
+        } else if (jasy.Env.isSet("debug")) {
+          throw new Error("Too many arguments!");
+        }
+      }
+      else
+      {
+        var view = new construct(this);
+      }
+      
+      return this.addView(name, view);
+    },
+
+
+
+
+
+
+    /*
+    ======================================================
       CHILDREN MANAGMENT
     ======================================================
     */
 
+    /**
+     * Returns a child presenter by its @name {String}.
+     */
     getChild : function(name) {
       return this.__children[name];
     },
 
+
+    /**
+     * Adds the given @presenter {Object} instance to the child registry 
+     * by the given @name {String}.
+     */
     addChild : function(name, presenter) 
     {
       var db = this.__children;
@@ -79,7 +178,7 @@ core.Class("core.mvc.presenter.Abstract",
           throw new Error("Child name " + name + " is already in use!");  
         }
         
-        if (!core.Main.isType(presenter, "Object")) {
+        if (!core.Main.isTypeOf(presenter, "Object")) {
           throw new Error("Invalid presenter instance: " + presenter);
         }
       }
@@ -92,8 +191,25 @@ core.Class("core.mvc.presenter.Abstract",
     /**
      * {Boolean} Removes the given child by its @name {String}. Returns whether it succeeded.
      */
-    removeChild : function(name) {
+    removeChildByName : function(name) {
       return delete this.__children[name];
+    },
+
+
+    /**
+     * {Boolean} Removes the given @child {Object}. Returns whether it succeeded.
+     */
+    removeChild : function(child) 
+    {
+      var db = this.__children;
+      for (var name in db) 
+      {
+        if (db[name] === child) {
+          return delete db[name];
+        }
+      }
+
+      return false;
     },
 
 

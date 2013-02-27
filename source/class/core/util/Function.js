79df879be3b0d3653879ab3640cceee9e1aacffe
Sebastian Werner <info@sebastian-werner.net>
2013-02-27 12:33:26 +0100

    Moved function extensions into utility class for easier dependency handling

--- source/class/core/util/Function.js
+++ source/class/core/util/Function.js
@@ -31,7 +31,70 @@
       return object[boundName] || (
         object[boundName] = func.bind(object)
       );
-    }
+    },
+
+
+    /**
+     * {Function} Debounces the given method.
+     *
+     * Debouncing ensures that exactly one signal is sent for an event that may be happening
+     * several times — or even several hundreds of times over an extended period. As long as
+     * the events are occurring fast enough to happen at least once in every detection
+     * period, the signal will not be sent!
+     *
+     * - @threshold {Integer} Number of milliseconds of distance required before reacting/resetting.
+     * - @execAsap {Boolean?false} Whether the execution should happen at begin.
+     */
+    debounce : function(func, threshold, execAsap)
+    {
+      // Via: http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
+      var timeout;
+
+      return function()
+      {
+        var obj = this, args = arguments;
+        function delayed()
+        {
+          if (!execAsap) {
+            func.apply(obj, args);
+          }
+
+          timeout = null;
+        };
+
+        if (timeout){
+          clearTimeout(timeout);
+        } else if (execAsap) {
+          func.apply(obj, args);
+        }
+
+        timeout = setTimeout(delayed, threshold || 100);
+      };
+    },
+    
+    
+    /**
+     * {Function} Returns a new function that when called multiple times will only call the 
+     * original function after the specificed @time {Integer} in milliseconds has elapsed.
+     */
+    throttle : function(func, time) 
+    {
+      var lastEventTimestamp = null;
+      var limit = time;
+
+      return function() 
+      {
+        var self = this;
+        var args = arguments;
+        var now = Date.now();
+
+        if (!lastEventTimestamp || now - lastEventTimestamp >= limit) 
+        {
+          lastEventTimestamp = now;
+          func.apply(self, args);
+        }
+      };
+    }    
 
   });  
 })();

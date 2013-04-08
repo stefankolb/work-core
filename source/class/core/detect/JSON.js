e000d44e112814809abdabbc268ddd36b32d162b
Sebastian Werner <info@sebastian-werner.net>
2013-04-08 19:44:33 +0200

    Cleanup date checks

--- source/class/core/detect/JSON.js
+++ source/class/core/detect/JSON.js
@@ -14,20 +14,7 @@
 
   var serialized = '{"A":[1,true,false,null,"\\u0000\\b\\n\\f\\r\\t"]}';
 
-  // Test the `Date#getUTC*` methods. Based on work by @Yaffle.
-  var isExtended = new Date(-3509827334573292);
 
-  try 
-  {
-    // The `getUTCFullYear`, `Month`, and `Date` methods return nonsensical
-    // results for certain dates in Opera >= 10.53.
-    isExtended = isExtended.getUTCFullYear() == -109252 && isExtended.getUTCMonth() === 0 && isExtended.getUTCDate() == 1 &&
-      // Safari < 2.0.2 stores the internal millisecond time value correctly,
-      // but clips the values returned by the date methods to the range of
-      // signed 32-bit integers ([-2 ** 31, 2 ** 31 - 1]).
-      isExtended.getUTCHours() == 10 && isExtended.getUTCMinutes() == 37 && isExtended.getUTCSeconds() == 6 && isExtended.getUTCMilliseconds() == 708;
-  } 
-  catch (exception) {}
 
   var PARSE = "parse";
   var STRINGIFY = "stringify";
@@ -89,24 +76,30 @@
       try 
       {
         stringifySupported =
+          
           // Firefox 3.1b1 and b2 serialize string, number, and boolean
           // primitives as object literals.
           JSON[STRINGIFY](0) === "0" &&
+          
           // FF 3.1b1, b2, and JSON 2 serialize wrapped primitives as object
           // literals.
           JSON[STRINGIFY](new Number()) === "0" &&
           JSON[STRINGIFY](new String()) == '""' &&
+          
           // FF 3.1b1, 2 throw an error if the value is `null`, `undefined`, or
           // does not define a canonical JSON representation (this applies to
           // objects with `toJSON` properties as well, *unless* they are nested
           // within an object or array).
           JSON[STRINGIFY](getClass) === undef &&
+          
           // IE 8 serializes `undefined` as `"undefined"`. Safari <= 5.1.7 and
           // FF 3.1b3 pass this test.
           JSON[STRINGIFY](undef) === undef &&
-          // Safari <= 5.1.7 and FF 3.1b3 throw `Error`s and `TypeError`s,
+          
+          // Safari < 7 (?) and FF 3.1b3 throw `Error`s and `TypeError`s,
           // respectively, if the value is omitted entirely.
           JSON[STRINGIFY]() === undef &&
+          
           // FF 3.1b1, 2 throw an error if the given value is not a number,
           // string, array, object, Boolean, or `null` literal. This applies to
           // objects with custom `toJSON` methods as well, unless they are nested
@@ -114,31 +107,33 @@
           // methods entirely.
           JSON[STRINGIFY](value) === "1" &&
           JSON[STRINGIFY]([value]) == "[1]" &&
-          // Prototype <= 1.6.1 serializes `[undefined]` as `"[]"` instead of
-          // `"[null]"`.
-          JSON[STRINGIFY]([undef]) == "[null]" &&
-          // YUI 3.0.0b1 fails to serialize `null` literals.
-          JSON[STRINGIFY](null) == "null" &&
+
           // FF 3.1b1, 2 halts serialization if an array contains a function:
           // `[1, true, getClass, 1]` serializes as "[1,true,],". These versions
           // of Firefox also allow trailing commas in JSON objects and arrays.
           // FF 3.1b3 elides non-JSON values from objects and arrays, unless they
           // define custom `toJSON` methods.
           JSON[STRINGIFY]([undef, getClass, null]) == "[null,null,null]" &&
+          
           // Simple serialization test. FF 3.1b1 uses Unicode escape sequences
           // where character escape codes are expected (e.g., `\b` => `\u0008`).
           JSON[STRINGIFY]({ "A": [value, true, false, null, "\0\b\n\f\r\t"] }) == serialized &&
+          
           // FF 3.1b1 and b2 ignore the `filter` and `width` arguments.
           JSON[STRINGIFY](null, value) === "1" &&
           JSON[STRINGIFY]([1, 2], null, 1) == "[\n 1,\n 2\n]" &&
+          
           // JSON 2, Prototype <= 1.7, and older WebKit builds incorrectly
           // serialize extended years.
           JSON[STRINGIFY](new Date(-8.64e15)) == '"-271821-04-20T00:00:00.000Z"' &&
+          
           // The milliseconds are optional in ES 5, but required in 5.1.
           JSON[STRINGIFY](new Date(8.64e15)) == '"+275760-09-13T00:00:00.000Z"' &&
+          
           // Firefox <= 11.0 incorrectly serializes years prior to 0 as negative
           // four-digit years instead of six-digit years. Credits: @Yaffle.
           JSON[STRINGIFY](new Date(-621987552e5)) == '"-000001-01-01T00:00:00.000Z"' &&
+          
           // Safari <= 5.1.5 and Opera >= 10.53 incorrectly serialize millisecond
           // values less than 1000. Credits: @Yaffle.
           JSON[STRINGIFY](new Date(-1)) == '"1969-12-31T23:59:59.999Z"';
@@ -156,7 +151,6 @@
   {
     VALID_PARSE : parseSupported,
     VALID_STRINGIFY : stringifySupported,
-    VALID_DATE : isExtended,
 
     VALUE : parseSupported && stringifySupported
   });

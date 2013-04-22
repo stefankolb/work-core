6a69e49b1828ea1630faa1dc1981abc7ec7a6b8a
Sebastian Fastner <mail@sebastianfastner.de>
2013-04-22 15:07:27 +0200

    Stabelize base62 implementation

--- source/class/core/util/Base62.js
+++ source/class/core/util/Base62.js
@@ -15,134 +15,180 @@
 	for (var i=0; i<62; i++) {
 		base62InvertedTable[base62Table[i]] = i;
 	}
-	
+	var bitMask = [252, 252, 252, 248, 240, 224, 192, 128]; // And mask for 6, 6, 6, 5, 4, 3, 2, 1 leading bits
+
 	var encodeArrayOfBytes = function(arr) {
-		var inputLength = arr.length;
-		var bitlen = inputLength * 8;
-		var ret = [];
-		
-		var bitpos = 0;
-		var getBits = 6;
-		var specialBit = 0;
-		var leadingZero = 0;
-		var val;
-		while (bitpos < bitlen) {
-			var posMod8 = bitpos % 8;
-			console.log(bitpos, posMod8);
-			var posDiv8 = Math.floor(bitpos / 8);
-			if (getBits == 6) {
-				if (posMod8 < 2) {
-					val = (arr[posDiv8] >> (2 - posMod8)) & 63;
-				} else {
-					var nextPos = posDiv8 + 1;
-					if (nextPos >= inputLength) {
-						val = ((arr[posDiv8] << posMod8) & 255) >> (8-posMod8);
-					} else {
-						var nextBits = 6-(8-posMod8);
-						val = arr[nextPos] >> (8-nextBits);
-						val += arr[posDiv8] << nextBits;
-						val = val & 63;
-					}
-				}
-			} else {
-				if (posMod8 < 3) {
-					val = (arr[posDiv8] >> (3 - posMod8)) & 31;
-				} else {
-					var nextPos = posDiv8 + 1;
-					if (nextPos >= inputLength) {
-						val = ((arr[posDiv8] << posMod8) & 255) >> posMod8;
-						var bits = 8-posMod8;
-						val = val << 5-bits;
-						leadingZero = 6 - (bits + 1);
-						console.log(bits, (val).toString(2));
-					} else {
-						var nextBits = 5-(8-posMod8);
-						val = arr[nextPos] >> (8-nextBits);
-						val += arr[posDiv8] << nextBits;
-						val = val & 31;
-					}
-				}
-				val |= specialBit;
-				if (leadingZero) {
-					val = val >> leadingZero;
-					leadingZero = 0;
-				}
-				console.log("special", (val).toString(2));
-			}
-			
-			bitpos += getBits;
-			
-			if (val == 60 || val == 61) {
-				// Special case: bits are 111100 or 111101
-				getBits = 5;
-				specialBit = (val == 60) ? 0 : 32;
-				ret.push(base62Table[60]);
-			} else if (val == 62 || val == 63) {
-				// Special case: bits are 111110 or 111111
-				getBits = 5;
-				specialBit = (val == 62) ? 0 : 32;
-				ret.push(base62Table[61]);
-			} else {
-				getBits = 6;
-				ret.push(base62Table[val]);
-			}
-		}
-		
-		return ret.join("");
-	};
+		// This works like a bit register. Take first 6 bits and push it to result. Take next 6 bits and so on.
+		// A special case is if the 6 bits represents 60, 61, 62 or 63. In this case one more bit is used to
+		// reduce 6 bit (= 64 different values) by two values.
+		var result = [];
+		var charLength = arr.length;
+		var bitLength = charLength * 8;
+		var bitPos = 0;
+		var specialBit = null;
 	
-	var decodeToArrayOfBytes = function(str) {
-		var ret = [];
-		var cur = 0;
-		var bitpos = 0;
+		while (bitPos < bitLength) {
+			var charOffset = bitPos / 8 | 0;
+			var bitOffset = bitPos % 8;
 		
-		for (var i=0, ii=str.length; i<ii; i++) {
-			var byte = base62InvertedTable[str[i]];
-console.log(i, "bitpos: ", bitpos, cur.toString(2));
-			if (byte == 60 || byte == 61) {
-				cur = (byte == 60) ? 30 : 31;
-				bitpos = 5;
-			} else {
-				if (bitpos == 0) {
-					cur = byte;
-					bitpos = 6;
+			var extractedBits;
+			if (charOffset + 1 >= charLength) {
+				// Special case : no more next char so no more bits
+				var remainingBits = bitLength - bitPos;
+				var moveRight;
+				if (remainingBits >= 6) {
+					moveRight = 2;
 				} else {
-					var neededBits = 8-bitpos;
-					console.log(i, "needed", neededBits);
-					if (neededBits <= 6) {
-						var val;
-						if (i==ii-1) {
-							val = byte;
-						} else {
-							val = byte >> (6 - neededBits);
-						}
-						cur = (cur << neededBits) + val;
-						console.log(i, neededBits, byte.toString(2), val.toString(2), cur.toString(2));
-						ret.push(cur);
-						cur = ((byte << neededBits) & 63) >> neededBits;
-						bitpos = 6 - neededBits;
-					} else {
-						cur = (cur << 6) + byte;
-						console.log(i, cur.toString(2));
-						bitpos += 6;
+					moveRight = 8 - remainingBits;
+				}
+				extractedBits = ( (arr[charOffset] << bitOffset) & 252 ) >> moveRight;
+			} else {
+				var leftoverBits = bitOffset - 2;
+				extractedBits = (( (arr[charOffset] << bitOffset) & bitMask[bitOffset] )
+							+ ( (arr[charOffset+1] & bitMask[8-leftoverBits]) >> (6-leftoverBits) )) >> 2;
+			}
+		
+			if ((extractedBits & 62) == 60) {
+				extractedBits = 61;
+				bitPos -= 1;
+			} else if ((extractedBits & 62) == 62) {
+				extractedBits = 62;
+				bitPos -= 1;
+			}
+			result.push(extractedBits);
+		
+			bitPos += 6;
+		}
+	
+		return result;
+	};
+
+	var decodeToArrayOfBytes = function(arr) {
+		var result = [];
+		var current = 0;
+		var bitOffset = 0;
+		var charOffset = 0;
+		var charLength = arr.length;
+	
+		for (var charOffset=0; charOffset < charLength; charOffset++) {
+			var char = arr[charOffset];
+		
+			var bitsNeeded = 8 - bitOffset;
+			if (char == 61 || char == 62) {
+				var correctBits = (char == 61) ? 30 : 31;
+				if (bitsNeeded <= 5) {
+					current = ((current << bitsNeeded) + (correctBits >> (5-bitsNeeded))) & 255;
+					result.push(current);
+					current = (((correctBits << bitsNeeded) & 255) >> bitsNeeded) & 63;
+					bitOffset = 5 - bitsNeeded;
+				} else {
+					current = (current << 5) + correctBits;
+					bitOffset += 5;
+				
+					if (bitOffset == 8) {
+						result.push(current);
+						current = 0;
+					}
+				}
+			} else {
+				if (bitsNeeded <= 6) {
+					var last = charOffset == charLength -1;
+					var charShift = char;
+					if (!last) {
+						charShift = char >> (6-bitsNeeded);
+					}
+					current = ((current << bitsNeeded) + charShift) & 255;
+					result.push(current);
+					if (!last) {
+						current = (((char << bitsNeeded) & 255) >> bitsNeeded) & 63;
+					}
+					bitOffset = 6 - bitsNeeded;
+				} else {
+					current = (current << 6) + char;
+					bitOffset += 6;
+				
+					if (bitOffset == 8) {
+						result.push(current);
+						current = 0;
 					}
 				}
 			}
 		}
-		
-		return ret;
+	
+		return result;
 	};
 	
+	var encodeArrayToString = function(arr) {
+		var result = encodeArrayOfBytes(arr);
+		for (var i=0, ii=result.length; i<ii; i++) {
+			result[i] = base62Table[result[i]];
+		}
+		
+		return result.join("");
+	};
+	
+	var decodeStringToArray = function(str) {
+		var len = str.length;
+		var byteArray = new Array(len);
+		for (var i=0; i<len; i++) {
+			byteArray[i] = base62InvertedTable[str[i]];
+		}
+		return decodeToArrayOfBytes(byteArray);
+	};
+	
+	/**
+	 * Base62 encoder and decoder.
+	 * Most implementations out there encodes and decodes only one byte/character
+	 * and joins the result to one result string. This is wrong behaviour as the
+	 * resulting string is longer than needed.
+	 * This implementation supports bit packing as described in 
+	 * http://202.194.20.8/proc/ICCS2008/papers/156.pdf (A Secure, Lossless, and 
+	 * Compressed Base62 Encoding) / Section Base62 Encoding.
+	 */
 	core.Module("core.util.Base62", {
 		/**
-		 * {String} Returns an universally unique ID generated using version 4 algorithm
-		 * of http://www.ietf.org/rfc/rfc4122.txt
+		 * {String} Encodes @str {String} to base62 encoded string
 		 */
-		encode : function() {
+		encode : function(str) {
+			var len = str.length;
+			var byteArray = new Array(len);
+			for (var i=0; i<len; i++) {
+				byteArray[i] = str.charCodeAt(i);
+			}
 			
+			return encodeArrayToString(byteArray);
 		},
 		
-		decode : function() {
-		}
+		/**
+		 * {Array} Encodes @arr {Array} to base62 encoded array
+		 */
+		encodeArray : encodeArrayOfBytes,
+		
+		/**
+		 * {String} Encodes @arr {Array} to base62 encoded string.
+		 */
+		encodeArrayToString : encodeArrayToString,
+		
+		/**
+		 * {String} Decodes base62 encoded @str {String} to plain text string.
+		 */
+		decode : function(str) {
+			var result = decodeStringToArray(str);
+			for (var i=0, ii=result.length; i<ii; i++) {
+				result[i] = String.fromCharCode(result[i]);
+			}
+			
+			return result.join("");
+		},
+		
+		/**
+		 * {Array} Decodes base62 encoded @arr {Array}.
+		 */
+		decodeArray : decodeToArrayOfBytes,
+		
+		/**
+		 * {Array} Decodes base62 encoded @str {String}.
+		 */
+		decodeStringToArray : decodeStringToArray
 	});
 })();

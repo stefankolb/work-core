c8d51f774276e5f44388db979e7edb6fbc6d850c
Sebastian Werner <info@sebastian-werner.net>
2013-05-08 15:51:57 +0200

    Code improvements

--- source/class/fix/ArrayBugs.js
+++ source/class/fix/ArrayBugs.js
@@ -9,41 +9,46 @@
 ==================================================================================================
 */
 
-// ES5 15.4.4.12
-// http://es5.github.com/#x15.4.4.12
-// Default value for second param
-// IE < 9 bug: [1,2].splice(0).join("") == "" but should be "12"
-if ([1,2].splice(0).length != 2) 
+(function()
 {
-  var array_splice = Array.prototype.splice;
-  
-  Array.prototype.splice = function(start, deleteCount) 
+  var ArrayProto = Array.prototype;
+
+  // ES5 15.4.4.12
+  // http://es5.github.com/#x15.4.4.12
+  // Default value for second param
+  // IE < 9 bug: [1,2].splice(0).join("") == "" but should be "12"
+  if ([1,2].splice(0).length != 2) 
   {
-    if (!arguments.length) 
+    var array_splice = ArrayProto.splice;
+    
+    ArrayProto.splice = function(start, deleteCount) 
     {
-       return [];
-    }
-    else
-    {
-      return array_splice.apply(this, [
+      if (!arguments.length) 
+      {
+         return [];
+      }
+      else
+      {
+        return array_splice.apply(this, [
           start === void 0 ? 0 : start,
           deleteCount === void 0 ? (this.length - start) : deleteCount
-      ].concat(slice.call(arguments, 2)));
-    }
-  };
-}
+        ].concat(slice.call(arguments, 2)));
+      }
+    };
+  }
 
-// ES5 15.4.4.12
-// http://es5.github.com/#x15.4.4.13
-// Return len+argCount.
-// IE < 8 bug: [].unshift(0) == undefined but should be "1"
-if ([].unshift(0) != 1) 
-{
-  var array_unshift = Array.prototype.unshift;
-
-  Array.prototype.unshift = function() 
+  // ES5 15.4.4.12
+  // http://es5.github.com/#x15.4.4.13
+  // Return len+argCount.
+  // IE < 8 bug: [].unshift(0) == undefined but should be "1"
+  if ([].unshift(0) != 1) 
   {
-    array_unshift.apply(this, arguments);
-    return this.length;
-  };
-}
+    var array_unshift = ArrayProto.unshift;
+
+    ArrayProto.unshift = function() 
+    {
+      array_unshift.apply(this, arguments);
+      return this.length;
+    };
+  }
+})();

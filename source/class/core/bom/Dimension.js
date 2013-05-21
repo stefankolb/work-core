1dd3dd323c7262e561cd88f357f003c780ac7bd5
Sebastian Werner <info@sebastian-werner.net>
2013-05-21 18:02:31 +0200

    Added docs

--- source/class/core/bom/Dimension.js
+++ source/class/core/bom/Dimension.js
@@ -1,9 +1,26 @@
+/*
+==================================================================================================
+  Core - JavaScript Foundation
+  Copyright 2013 Sebastian Werner
+==================================================================================================
+*/
+
+/**
+ * API to query element dimensions
+ */
 core.Module("core.bom.Dimension",
 {
+  /**
+   * {Integer} Returns the inner width of @element {Element}.
+   */
   getInnerWidth : function(element) {
     return element.clientWidth - core.bom.Style.getInteger(element, "paddingLeft", true) - core.bom.Style.getInteger(element, "paddingRight", true);
   },
+  
 
+  /**
+   * {Integer} Returns the inner height of @element {Element}.
+   */
   getInnerHeight : function(element) {
     return element.clientHeight - core.bom.Style.getInteger(element, "paddingTop", true) - core.bom.Style.getInteger(element, "paddingBottom", true);
   }

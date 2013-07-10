d7cea8af5cdcaaebfd35866798835fffb5e578ab
Sebastian Werner <info@sebastian-werner.net>
2013-07-10 11:46:45 +0200

    Added NavigationPresenter for basic Path interaction

---
+++ source/class/core/mvc/presenter/NavigationPresenter.js
@@ -0,0 +1,36 @@
+core.Class("core.mvc.presenter.NavigationPresenter",
+{
+  include : [core.mvc.presenter.Abstract],
+
+  construct : function()
+  {
+    core.mvc.presenter.Abstract.call(this);
+
+    
+  },
+
+  members :
+  {
+    __param : null,
+    __segment : null,
+
+    setParam : function(param) {
+      this.__param = param;
+    },
+
+    getParam : function() {
+      return this.__param;
+    },
+
+    setSegment : function(segment) {
+      this.__segment = segment;
+    },
+
+    getSegment : function() {
+      return this.__segment;
+    }
+
+
+
+  }
+});

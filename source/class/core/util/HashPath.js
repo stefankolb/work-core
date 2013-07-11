a08af087eda564b8b7b2046c5c3b7759cd3af6b0
Sebastian Werner <info@sebastian-werner.net>
2013-07-11 11:58:19 +0200

    Added HashPath library which is pretty much a cleaned up modern approach to Unify's Path class.

---
+++ source/class/core/util/HashPath.js
@@ -0,0 +1,178 @@
+/*
+==================================================================================================
+  Core - JavaScript Foundation
+  Copyright 2013 Sebastian Werner
+--------------------------------------------------------------------------------------------------
+  Based on Unify <unify-project.org>
+  Copyright: 2009-2010 Deutsche Telekom AG
+==================================================================================================
+*/
+
+(function()
+{
+  /** {=RegExp} Matches location fragments (presenter.section:param) */
+  var fragmentMatcher = /^([a-z0-9-]+)?(\.([a-z-]+))?(\:([a-zA-Z0-9_%=-]+))?$/;
+
+  /**
+   * {Map} Parses a location fragment @fragment {String} into a object with the keys "presenter", "section" and "param".
+   */
+  var parseFragment = function(fragment)
+  {
+    var match = fragmentMatcher.exec(fragment);
+
+    if (jasy.Env.isSet("debug"))
+    {
+      if (!match) {
+        throw new Error("Invalid fragment: " + fragment);
+      }
+    }
+
+    return {
+      presenter : RegExp.$1 || null,
+      section : RegExp.$3 || null,
+      param : RegExp.$5 || null
+    };
+  };
+
+
+  /**
+   * A path handler ideally suited for managing the path inside the URI hash part.
+   *
+   * Supports a structure of presenters, sections and parameters.
+   */
+  core.Class("core.util.HashPath", 
+  {
+    pooling : true,
+
+    construct : function(path)
+    {
+      var data = this.__data;
+
+      // Support reusing existing array for pooling
+      if (!data) {
+        data = this.__data = [];  
+      } else {
+        data.length = 0;
+      }
+      
+      if (path && path.length > 0)
+      {
+        var fragments = path.split("/");
+        for (var i=0, l=fragments.length; i<l; i++) {
+          data.push(parseFragment(fragments[i]));
+        }
+      }    
+    },
+
+    members :
+    {
+      /**
+       * Returns the current fragment - the last one - of the path.
+       */
+      getCurrent : function()
+      {
+        var data = this.__data;
+        return data[data.length-1];
+      },
+
+
+      /**
+       * Compares this path to the given @other {core.util.HashPath}. Returns either:
+       *
+       * - jump: presenter change without detectable direction
+       * - out: movement one hierarchy up
+       * - in: movement one hierarchy down
+       * - param: parameter changed
+       * - section: section changed
+       * - equal: both paths are equal
+       *
+       * Please note: Only the first detected change is returned e.g. a path might have more differences
+       * than just the first different part.
+       */
+      compareTo : function(other)
+      {
+        if (this === other) {
+          return "equal";
+        }
+
+        var myData = this.__data;
+        var otherData = other.__data;
+
+        var maxLength = Math.max(myData.length, otherData.length);
+        for (var i=0; i<maxLength; i++)
+        {
+          var myFragment = myData[i];
+          var otherFragment = otherData[i];
+
+          // Length differences
+          if (!myFragment || !otherFragment)
+          {
+            if (i == 0) {
+              return "jump";
+            } else if (!myFragment) {
+              return "in";
+            } else if (!otherFragment) {
+              return "out";
+            }
+          }
+
+          // Fragment detail comparisions
+          if (myFragment.presenter != otherFragment.presenter) {
+            return "jump";
+          } else if (myFragment.param != otherFragment.param) {
+            return "param";
+          } else if (myFragment.section != otherFragment.section) {
+            return "section";
+          }
+        }
+
+        return "equal";
+      },
+
+
+      /**
+       * Deep clones a Path instance.
+       *
+       * @return {core.util.HashPath} Returns the deeply cloned Path instance.
+       */
+      clone : function()
+      {
+        var clone = core.util.HashPath.obtain();
+        clone.__data = this.__data.concat();
+        return clone;
+      },
+
+
+      /**
+       * Serializes a Path object into a location string. The result
+       * can be re-used for {@link core.util.HashPath.fromString}.
+       *
+       * @return {String} Location string
+       */
+      serialize : function()
+      {
+        var data = this.__data;
+        var result = [];
+
+        for (var i=0, l=data.length; i<l; i++)
+        {
+          var entry = this[i];
+          var temp = entry.presenter;
+
+          if (entry.section) {
+            temp += "." + entry.section;
+          }
+
+          if (entry.param) {
+            temp += ":" + entry.param;
+          }
+
+          result.push(temp);
+        }
+
+        return result.join("/");
+      }    
+    }
+  });  
+
+})();

8b6ece8ab359f1c9f1a0e89c24e42389b771733f
Sebastian Werner <info@sebastian-werner.net>
2013-07-16 17:12:40 +0200

    Added initial variant for Tap events

---
+++ source/class/core/bom/TapEvent.js
@@ -0,0 +1,39 @@
+/**
+ * #require(core.bom.PointerEvents2)
+ */
+core.Module("core.bom.TapEvent",
+{
+  init : function()
+  {
+    var downOn = null;
+    var downX = -1000;
+    var downY = -1000;
+    var maxClickMovement = 5;
+
+    document.addEventListener("pointerdown", function(e) 
+    {
+      if (!e.isPrimary) {
+        return;
+      }
+
+      downOn = e.target;
+      downX = e.offsetX;
+      downY = e.offsetY;
+
+      console.log("Down on: " + downOn, downX, downY)
+    }, true);
+
+    document.addEventListener("pointerup", function(e) 
+    {
+      if (!e.isPrimary) {
+        return;
+      }
+
+      if (e.target == downOn && Math.abs(downX - e.offsetX) < maxClickMovement && Math.abs(downY - e.offsetY) < maxClickMovement) 
+      {
+        
+
+      }
+    }, true);
+  }
+});

2a026d6e50126ec9772f60aa0683decc4c34164a
Sebastian Werner <info@sebastian-werner.net>
2013-07-16 14:43:03 +0200

    Added enableAppLayout() to update/tweak viewport for mobile browser apps

--- source/class/core/bom/Viewport.js
+++ source/class/core/bom/Viewport.js
@@ -1,10 +1,10 @@
 /*
 ==================================================================================================
-  Core - JavaScript Foundation
-  Copyright 2010-2012 Zynga Inc.
-  Copyright 2012-2013 Sebastian Werner
+	Core - JavaScript Foundation
+	Copyright 2010-2012 Zynga Inc.
+	Copyright 2012-2013 Sebastian Werner
 --------------------------------------------------------------------------------------------------
-  Inspired by: http://www.quirksmode.org/blog/archives/2011/12/outerwidth_and.html
+	Inspired by: http://www.quirksmode.org/blog/archives/2011/12/outerwidth_and.html
 ==================================================================================================
 */
 
@@ -36,6 +36,42 @@
 		/** {Boolean} Whether the viewport is in portrait orientation */
 		isPortrait: function() {
 			return window.outerWidth < window.outerHeight;
+		},
+
+		/**
+		 * Works the viewport magic on sites which do not like to offer native scrolling
+		 * but works with fixed components and custom scrolling. In these app like sites
+		 * we like to offer a view without the top browser bar. This is possible and will
+		 * be applied by this method. The method also offers features to make 
+		 */
+		enableAppLayout : function()
+		{
+			var helper = function()
+			{
+				// - First enforce height on documentElement to make document larger and allow scrolling
+				// - Scroll to any location (will hide the menu bar)
+				// - Sync body height with actual window inner height (now without the top bar)
+				document.documentElement.style.height = "5000px";
+				window.scrollTo(0, 0);
+				document.body.style.height = window.innerHeight + "px";
+			};
+
+			// Execute on every orientation change
+			window.addEventListener('orientationchange', function() {
+				helper();
+			}, false);
+
+			// Prevent native touch start handling to disable native scrolling
+			window.addEventListener("touchstart", function(e) 
+			{
+				var target = e.target.tagName;
+				if (target != "INPUT" && target != "SELECT" && target != "TEXTAREA") {
+					e.preventDefault();	
+				}
+			}, true);
+
+			// Directly execute for the first time
+			helper();
 		},
 		
 		/** {String} Returns the viewport orientation. */

6ff0d58ca809423257b94af8d29f1e81b0254a63
Sebastian Werner <info@sebastian-werner.net>
2013-08-07 15:49:32 +0200

    Improved form get data handling to actually correctly auto deliver the expected result

--- source/class/core/bom/Form.js
+++ source/class/core/bom/Form.js
@@ -27,37 +27,79 @@ core.Module("core.bom.Form",
 
 		var elems = form.elements;
 		var result = {};
+		var boollike = {};
 		var undef;
 
 		for (var i=0, l=elems.length; i<l; i++)
 		{
 			var item = elems[i];
 			var name = item.name;
+			var value = core.bom.FormItem.getValue(item);
 
+			// Only keep successful items
 			if (core.bom.FormItem.isSuccessful(item)) 
 			{
-				var value = core.bom.FormItem.getValue(item);
 				var stored = result[name];
 
-				if (stored !== undef) 
+			  // Always overwrite when it is a list value from e.g. a multi select field
+				if (value instanceof Array) {
+					result[name] = value;	
+				}
+
+				// Overwrite when current value is undefined
+			  else if (stored === undef) 
+			  {
+			  	// Behave differently on whether the key already existed or not:
+			  	// Check whether we expect an array return value
+			  	// which is true whenever two fields of the same name exist
+			  	if (name in result && core.bom.FormItem.isMulti(item)) 
+			  	{
+			  		result[name] = [value];
+			  		boollike[name] = false;
+			  	}
+			  	else
+			  	{
+			  		result[name] = value;
+			  	}
+			  }
+
+				// If there is already something stored (e.g. multiple checkboxes with the same name)
+				// we simple push to the list of values or create that list from the other value
+				else if (stored instanceof Array) 
 				{
-					if (stored instanceof Array) {
-						stored.push(value);
-					} else {
-						result[name] = [stored, value];
-					}
+					stored.push(value);
 				}
 				else
 				{
-					result[name]	= value;	
+					result[name] = [stored, value];
+					boollike[name] = false;
 				}
 			}
+
+			// Store undefined for the first value. Mainly for checkboxes
+			// where non is enabled and to pass any value/key at all.
 			else if (!(name in result))
 			{
-				// Relevant for checkboxes which are about enabling a specific flag
-				// This way these are available as key only and can be passed to 
-				// the property system etc.
-				result[name] = undef;
+				result[name] = undef;	
+				if (typeof value !== "boolean") {
+					boollike[name] = false;
+				}
+			}
+
+			// If we find the second form item with the same name we
+			// automatically translate the value to an array
+			else if (result[name] !== undef && (!result[name] instanceof Array))
+			{
+				result[name] = [result[name]];
+			}
+		}
+
+		// Automatically translate checkboxes which have a unique name
+		// and are not checked and have a truish value to a falsy value
+		for (var name in result)
+		{
+			if (result[name] === undef && boollike[name] !== false) {
+				result[name] = false;
 			}
 		}
 

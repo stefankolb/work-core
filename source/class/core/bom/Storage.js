e59b4f2fd8d00cba57722695d7ee8d7edf9259b6
Sebastian Werner <info@sebastian-werner.net>
2013-08-08 15:25:10 +0200

    Added plain storage wrapper for localStorage with additional support for type coercing from/to JSON + integrated text compression.

---
+++ source/class/core/bom/Storage.js
@@ -0,0 +1,75 @@
+/*
+==================================================================================================
+  Core - JavaScript Foundation
+  Copyright 2013 Sebastian Werner
+==================================================================================================
+*/
+
+"use strict";
+
+(function(global)
+{
+  var storage = global.localStorage;
+
+  /**
+   * Wrapped Storage API to store small data fragments on the client
+   */
+  core.Module("core.bom.Storage",
+  {
+    set : function(key, value)
+    {
+      if (jasy.Env.isSet("debug"))
+      {
+        core.Assert.isType(key, "String");
+        core.Assert.isType(value, "Plain", "Invalid data type to store!");
+      }
+
+      // Auto cast JSON objects and mark as JSON
+      if (typeof value == "object") {
+        value = "@J@" + core.JSON.stringify(value);
+      }
+      
+      var text = core.JSON.stringify(value);
+      var compressed = core.util.TextCompressor.compress(text);
+
+      storage.setItem("checkout", compressed);
+    },
+
+
+    get : function(key)
+    {
+      if (jasy.Env.isSet("debug")) {
+        core.Assert.isType(key, "String");
+      }
+
+      var compressed = storage.getItem(key, compressed);
+      if (compressed == null) {
+        return compressed;
+      }
+
+      var text = core.util.TextCompressor.decompress(compressed);
+
+      var type = text.slice(0,3);
+      if (type == "@J@") {
+        var value = JSON.parse(text.slice(3));
+      } else {
+        var value = text;
+      }
+
+      return value;
+    },
+
+
+    remove : function(key)
+    {
+      if (jasy.Env.isSet("debug")) {
+        core.Assert.isType(key, "String");
+      }
+
+      storage.removeItem(key);
+    }
+
+  });
+
+
+})(core.Main.getGlobal());

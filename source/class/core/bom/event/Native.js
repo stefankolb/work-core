808b4a85803eaf98fddb03464512de8a30001dad
Sebastian Werner <info@sebastian-werner.net>
2013-08-13 11:29:39 +0200

    Added new native event util

---
+++ source/class/core/bom/event/Native.js
@@ -0,0 +1,31 @@
+/*
+==================================================================================================
+  Core - JavaScript Foundation
+  Copyright 2013 Sebastian Werner
+==================================================================================================
+*/
+
+"use strict";
+
+/**
+ * Tools for native event handling
+ */
+core.Module("core.event.Native",
+{
+  fireElementEvent : fireEvent(element, event)
+  {
+    if (document.createEventObject)
+    {
+      // Dispatch for IE
+      var evt = document.createEventObject();
+      return element.fireEvent('on'+event,evt)
+    }
+    else
+    {
+      // Dispatch for standard
+      var evt = document.createEvent("HTMLEvents");
+      evt.initEvent(event, true, true ); // event type,bubbling,cancelable
+      return !element.dispatchEvent(evt);
+    }
+  }
+});

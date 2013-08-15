7d95f7548307e286108da725c6cbd5d902ef6a8e
Sebastian Werner <info@sebastian-werner.net>
2013-08-15 10:44:25 +0200

    Another test

--- source/class/core/event/Flow.js
+++ source/class/core/event/Flow.js
@@ -237,7 +237,7 @@
     {
       if (jasy.Env.isSet("debug")) 
       {
-        //core.Assert.isType(tasks, "Array");
+        core.Assert.isType(tasks, "Array");
 
         if (context != null) {
           core.Assert.isType(context, "Object");

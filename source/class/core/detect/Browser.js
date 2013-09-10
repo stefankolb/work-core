7091e8eea5fc4ad644250303b15654d243892a0e
Sebastian Werner <info@sebastian-werner.net>
2013-09-10 13:55:55 +0200

    Worked on browser detection

--- source/class/core/detect/Browser.js
+++ source/class/core/detect/Browser.js
@@ -1,2 +1,81 @@
-// Chrome == Boolean(window.chrome)
-// JSC == Safari == !/\n{2,}/.test(Function())
+/*
+==================================================================================================
+  Core - JavaScript Foundation
+  Copyright 2013 Sebastian Werner
+==================================================================================================
+*/
+
+"use strict";
+
+(function(global, RegExp)
+{
+  var name = jasy.Env.getValue("engine");
+  var version = null;
+  var agent = navigator.userAgent.toLowerCase();
+
+  if (jasy.Env.isSet("engine", "trident")) 
+  {
+    if (/(msie) ([0-9.]+)/.exec(agent))
+    {
+      name = "ie";
+      version = RegExp.$2;
+    }    
+  }
+  else if (jasy.Env.isSet("engine", "gecko"))
+  {
+    if (/(firefox)\/([0-9.]+)/.exec(agent))
+    {
+      name = RegExp.$1;
+      version = RegExp.$2;
+    }
+  }
+  else if (jasy.Env.isSet("engine", "webkit"))
+  {
+    // Behavior test first (differences between JS engines V8 vs. Nitro)
+    if (!/\n{2,}/.test(Function()) && /version\/([0-9.]+)/.exec(agent)) 
+    {
+      name = "safari";
+      version = RegExp.$1;
+    }
+    else if ((global.chrome || global.chromium) && (/opr\/([0-9.]+)/.exec(agent) || /(chrome)\/([0-9.]+)/.exec(agent)))
+    {
+      if (RegExp.$2) 
+      {
+        name = "chrome";
+        version = RegExp.$2;
+      }
+      else
+      {
+        name = "opera";
+        version = RegExp.$1;
+      }
+    }
+  }
+  else if (jasy.Env.isSet("engine", "presto") && (/version\/([0-9.]+)/.exec(agent) || /opera ([0-9.]+)/.exec(agent)))
+  {
+    name = "opera";
+    version = RegExp.$1;
+  }
+
+  var value = name;
+  var major = null;
+
+  if (version != null && version != "") 
+  {
+    major = parseInt(version || "", 10);
+    value += " " + major;
+  }
+
+
+  /**
+   * Browser version info
+   */
+  core.Module("core.detect.Browser", 
+  {
+    VALUE : value,
+    VERSION : major,
+    FULLVERSION : version,
+    NAME : name
+  });
+})(core.Main.getGlobal(), RegExp);
+  

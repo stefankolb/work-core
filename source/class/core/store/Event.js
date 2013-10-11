cc245f552e2733a995d5189e77fdf852fa4783c7
Sebastian Fastner <mail@sebastianfastner.de>
2013-10-11 17:33:11 +0200

    Add new abstract store implementation This new store implementation is based upon promises and delivers a fine grained asynchronous data handling. Due to the use of promises a strict error handling is forced.

--- source/class/core/store/Event.js
+++ source/class/core/store/Event.js
@@ -1,7 +1,7 @@
 /*
 ==================================================================================================
   Core - JavaScript Foundation
-  Copyright 2013 Sebastian Werner
+  Copyright 2013 Sebastian Fastner
 ==================================================================================================
 */
 
@@ -13,76 +13,67 @@
  */
 core.Class("core.store.Event",
 {
-  pooling : true,
-  include : [core.event.MDispatchable],
-  implement : [core.event.IEvent],
+	pooling : true,
+	include : [core.event.MDispatchable],
+	implement : [core.event.IEvent],
 
-  /**
-   * @type {String} Type of the event e.g. `click`, `load`, ...
-   * @success {Boolean} Whether the reason for the event was a success or failure.
-   * @item {var?null} Item ID to attach to the event.
-   * @data {var?null} Data to be attached to the event.
-   * @message {String?null} Message for user feedback etc.
-   */
-  construct : function(type, success, item, data, message) 
-  {
-    if (jasy.Env.isSet("debug"))
-    {
-      core.Assert.isType(type, "String", "Invalid event type!" + type + " :: " + typeof type);
-      core.Assert.isType(success, "Boolean", "Invalid event success state!");
+	/**
+	 * @type {String} Type of the event e.g. `click`, `load`, ...
+	 * @success {Boolean} Whether the reason for the event was a success or failure.
+	 * @data {var?null} Data to be attached to the event.
+	 * @message {String?null} Message for user feedback etc.
+	 */
+	construct : function(type, success, data, message)
+	{
+		if (jasy.Env.isSet("debug"))
+		{
+			core.Assert.isType(type, "String", "Invalid event type!" + type + " :: " + typeof type);
+			core.Assert.isType(success, "Boolean", "Invalid event success state!");
 
-      if (message != null) {
-        core.Assert.isType(message, "String", "Invalid event message!");
-      }
-    }
+			if (message != null) {
+				core.Assert.isType(message, "String", "Invalid event message!");
+			}
+		}
 
-    this.__type = type;
-    this.__success = success;
-    this.__item = item;
-    this.__data = data;
-    this.__message = message;
-  },
+		this.__type = type;
+		this.__success = success;
+		this.__data = data;
+		this.__message = message;
+	},
 
-  members :
-  {  
-    // Interface implementation
-    getType : function() {
-      return this.__type;
-    },
+	members :
+	{
+		// Interface implementation
+		getType : function() {
+			return this.__type;
+		},
 
-    /**
-     * {var} Returns whether the reason for the event is a success.
-     */
-    isSuccessful : function() {
-      return this.__success;
-    },
+		/**
+		 * {var} Returns whether the reason for the event is a success.
+		 */
+		isSuccessful : function() {
+			return this.__success;
+		},
 
-    /**
-     * {var} Returns whether the reason for the event is a failure..
-     */
-    isErrornous : function() {
-      return !this.__success;
-    },    
+		/**
+		 * {var} Returns whether the reason for the event is a failure..
+		 */
+		isErrornous : function() {
+			return !this.__success;
+		},
 
-    /**
-     * {var} Returns the item ID attached to the event.
-     */
-    getItem : function() {
-      return this.__item;
-    },
+		/**
+		 * {var} Returns the data attached to the event.
+		 */
+		getData : function() {
+			return this.__item;
+		},
 
-    /**
-     * {var} Returns the data attached to the event.
-     */
-    getData : function() {
-      return this.__data;
-    },
-
-    /**
-     * {String} Returns the message attached to the event.
-     */
-    getMessage : function() {
-      return this.__message;
-    }
-  }
+		/**
+		 * {String} Returns the message attached to the event.
+		 */
+		getMessage : function() {
+			return this.__message;
+		}
+	}
 });

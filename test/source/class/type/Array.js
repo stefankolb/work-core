1a6742b2fb7ee5f901ed830a5467b40915183dd9
Sebastian Werner <info@sebastian-werner.net>
2013-10-29 16:11:35 +0100

    Fixed test suite

--- test/source/class/type/Array.js
+++ test/source/class/type/Array.js
@@ -1,6 +1,6 @@
 var suite = new core.testrunner.Suite("Type/Array");
 
-suite.test("at", function() 
+suite.test("at", function()
 {
   var arr = [1,2,3,4,5];
   this.isEqual(core.Array.at(arr, 0), 1);
@@ -9,7 +9,7 @@ suite.test("at", function()
   this.isEqual(core.Array.at(arr, -20));
 });
 
-suite.test("clone", function() 
+suite.test("clone", function()
 {
   var orig = [1,2,3];
   var clone = core.Array.clone(orig);
@@ -22,7 +22,7 @@ suite.test("clone", function()
   this.isEqual(orig.join(","), clone.join(","));
 });
 
-suite.test("compact", function() 
+suite.test("compact", function()
 {
   var sparse = [1,2,3,,5,,,8];
   this.isEqual(core.Array.compact(sparse).length, 5);
@@ -32,13 +32,13 @@ suite.test("compact", function()
   this.isEqual(core.Array.compact(sparse).length, 7);
 });
 
-suite.test("contains", function() 
+suite.test("contains", function()
 {
   var arr1 = [1,2,3,5,6,7];
   this.isTrue(core.Array.contains(arr1, 3));
   this.isTrue(!core.Array.contains(arr1, 4));
   this.isTrue(core.Array.contains(arr1, 5));
-  
+
   var arr2 = ["true","1",3,false];
   this.isTrue(!core.Array.contains(arr2, true));
   this.isTrue(!core.Array.contains(arr2, 1));
@@ -51,7 +51,7 @@ suite.test("contains", function()
   this.isTrue(core.Array.contains(arr2, false));
 });
 
-suite.test("every", function() 
+suite.test("every", function()
 {
   this.isTrue(core.Array.every([], function() {}));
 
@@ -64,7 +64,7 @@ suite.test("every", function()
   }));
 });
 
-suite.test("flatten", function() 
+suite.test("flatten", function()
 {
   this.isEqual(core.Array.flatten([[1], 2, [3]]).toString(), [1,2,3].toString());
   this.isEqual(core.Array.flatten([["a"],[],"b","c"]).toString(), ["a","b","c"].toString());
@@ -83,7 +83,7 @@ suite.test("filter", function()
     return value == index;
   });
 
-  this.isEqual(input.join(","), "0,1,2,3,4,5,6");  
+  this.isEqual(input.join(","), "0,1,2,3,4,5,6");
   this.isNotIdentical(input, output);
 });
 
@@ -105,10 +105,10 @@ suite.test("forEach", function()
     test.isIdentical(array, arrayref);
     test.isIdentical(index, counter++);
   }, context);
-  this.isEqual(counter, 3);  
+  this.isEqual(counter, 3);
 });
 
-suite.test("fromArguments", function() 
+suite.test("fromArguments", function()
 {
   var test = this;
 
@@ -125,7 +125,7 @@ suite.test("fromArguments", function()
   })(1,2,3);
 });
 
-suite.test("insertAt", function() 
+suite.test("insertAt", function()
 {
   var arr1 = [1,2,3,4,5,6,7];
   this.isEqual(core.Array.insertAt(arr1, "end"), "end");
@@ -144,13 +144,13 @@ suite.test("insertAt", function()
   this.isEqual(arr1.join(","), "1,2,3,4,fromEnd,5,6,7");
 });
 
-suite.test("last", function() 
+suite.test("last", function()
 {
   var arr = [1,2,3,4,5];
   this.isEqual(core.Array.last(arr), 5);
 });
 
-suite.test("map", function() 
+suite.test("map", function()
 {
   this.isEqual(core.Array.map([1,2,3], function(value) {
     return value * 2;
@@ -164,7 +164,7 @@ suite.test("map", function()
     return value + array[array.length-1-index];
   }).join(","), [4,4,4].join(","));
 
-  var obj = 
+  var obj =
   {
     data : [20,30,40],
     mapper : function(value, index) {
@@ -175,27 +175,21 @@ suite.test("map", function()
   this.isEqual(core.Array.map([2,3,4], obj.mapper, obj).join(","), "22,33,44");
 });
 
-suite.test("max", function() 
+suite.test("max", function()
 {
   this.isEqual(core.Array.max([1,4,23,3]), 23);
   this.isEqual(core.Array.max([10,10,10]), 10);
   this.isEqual(core.Array.max([]), -Infinity);
 });
 
-suite.test("min", function() 
+suite.test("min", function()
 {
   this.isEqual(core.Array.min([1,4,23,3]), 1);
   this.isEqual(core.Array.min([10,10,10]), 10);
   this.isEqual(core.Array.min([]), Infinity);
 });
 
-suite.test("randomize", function() 
-{
-  var arr1 = [1,4,23,3,4,54,94,23,25,236];
-  core.Array.randomize(arr1);
-});
-
-suite.test("remove", function() 
+suite.test("remove", function()
 {
   var arr = [1,2,3,4,5,6];
   this.isEqual(core.Array.remove(arr, 4), 4);
@@ -208,7 +202,7 @@ suite.test("remove", function()
   this.isEqual(arr.join(","), "1,2,1,2,3");
 });
 
-suite.test("removeAt", function() 
+suite.test("removeAt", function()
 {
   var arr = [1,2,3,4,5,6];
   this.isEqual(core.Array.removeAt(arr, 2), 3);
@@ -216,7 +210,7 @@ suite.test("removeAt", function()
   this.isEqual(arr.join(","), "1,2,4,5,6");
 });
 
-suite.test("removeRange", function() 
+suite.test("removeRange", function()
 {
   var arr = [1,2,3,4,5,6,7,8,9];
   core.Array.removeRange(arr, 1, 1);
@@ -240,7 +234,7 @@ suite.test("removeRange", function()
   this.isEqual(arr.join(","), "1,,3,4");
 });
 
-suite.test("some", function() 
+suite.test("some", function()
 {
   this.isFalse(core.Array.some([], function() {}));
 
@@ -253,7 +247,7 @@ suite.test("some", function()
   }));
 });
 
-suite.test("sum", function() 
+suite.test("sum", function()
 {
   this.isEqual(core.Array.sum([1,4,23,3]), 31);
   this.isEqual(core.Array.sum([1,4,23,,,3]), 31);
@@ -265,8 +259,8 @@ suite.test("toKeys", function()
   this.isEqual(JSON.stringify(core.Array.toKeys(["foo","bar","baz"])), '{"foo":true,"bar":true,"baz":true}');
   this.isEqual(JSON.stringify(core.Array.toKeys(["foo","bar","baz"], 42)), '{"foo":42,"bar":42,"baz":42}');
 });
-  
-suite.test("unique", function() 
+
+suite.test("unique", function()
 {
   var arr = [1,2,3,1,2,3];
   this.isEqual(core.Array.unique(arr).join(","), "1,2,3");
@@ -282,11 +276,11 @@ suite.test("unique", function()
   this.isEqual(typeof unique[0], "number");
   this.isEqual(typeof unique[1], "string");
   this.isEqual(typeof unique[2], "number");
-  
+
   // does not support objects
   var arr = [{},{},{}];
   this.isEqual(core.Array.unique(arr).join(","), "[object Object]");
-  
+
   // but can work with special objects
   var hashCode = 0;
   var Special = function() {
@@ -299,7 +293,7 @@ suite.test("unique", function()
   this.isEqual(core.Array.unique(arr).join(","), "[object Special#0],[object Special#1],[object Special#2]");
 });
 
-suite.test("zip", function() 
+suite.test("zip", function()
 {
   var merged = core.Array.zip(["a","b","c"], [1,2,3]);
   this.isEqual(JSON.stringify(merged), '{"a":1,"b":2,"c":3}');

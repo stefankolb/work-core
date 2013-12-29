928bc80b9b61f333d7df71e34ae16ac04cb92a7e
Sebastian Werner <info@sebastian-werner.net>
2013-12-29 12:21:44 +0100

    Cleanup of tests for not anymore testable scenarios

--- test/source/class/jasy/Asset.js
+++ test/source/class/jasy/Asset.js
@@ -1,8 +1,8 @@
 var suite = new core.testrunner.Suite("Jasy/Asset");
 
-suite.test("Adding Source Data", function() 
+suite.test("Adding Source Data", function()
 {
-  this.raisesException(function() 
+  this.raisesException(function()
   {
     jasy.Asset.resetData();
     jasy.Asset.toUri("my.png")
@@ -12,51 +12,14 @@ suite.test("Adding Source Data", function()
   jasy.Asset.addData(
   {
     "assets" : {
-      "my.png" : {"u":"asset/my.png","p":0}
-    }, 
-    "profiles" : [{name:"source"}]
+      "my.png" : {"u":"asset/my.png"}
+    }
   });
   this.isEqual(jasy.Asset.toUri("my.png"), "asset/my.png");
-
-  
-  jasy.Asset.resetData();
-  jasy.Asset.addData(
-  {
-    "assets" : {
-      "my.png" : {"u":"asset/my.png","p":0}
-    }, 
-    "profiles" : [{name:"source", "root":"xxx/yyy/"}]
-  });
-  this.isEqual(jasy.Asset.toUri("my.png"), "xxx/yyy/asset/my.png");
-
-
-  jasy.Asset.resetData();
-  jasy.Asset.addData(
-  {
-    "assets" : {
-      "my.png" : {"u":"asset/my.png","p":0}
-    }, 
-    "profiles" : [{name:"source", "root":"http://mycdn.com/xxx/yyy/"}]
-  });
-  this.isEqual(jasy.Asset.toUri("my.png"), "http://mycdn.com/xxx/yyy/asset/my.png");
-  
-  
-  jasy.Asset.resetData();
-  jasy.Asset.addData(
-  {
-    "assets" : {
-      "lib2" : {
-        "my.png" : {"u":"../../lib2/asset/my.png","p":0}
-      }
-    }, 
-    "profiles" : [{name:"source", "root":"http://mycdn.com/app/source/"}]
-  });
-  this.isEqual(jasy.Asset.toUri("lib2/my.png"), "http://mycdn.com/app/source/../../lib2/asset/my.png");
-
 });
 
 
-suite.test("Adding Build Data", function() 
+suite.test("Adding Build Data", function()
 {
   this.raisesException(function() {
     jasy.Asset.resetData();
@@ -67,33 +30,10 @@ suite.test("Adding Build Data", function()
   jasy.Asset.addData(
   {
     "assets" : {
-      "my.png" : {"p":0}
-    }, 
-    "profiles" : [{name:"build", "root":"asset/"}]
+      "my.png" : {}
+    }
   });
   this.isEqual(jasy.Asset.toUri("my.png"), "asset/my.png");
 
-  
-  jasy.Asset.resetData();
-  jasy.Asset.addData(
-  {
-    "assets" : {
-      "my.png" : {"p":0}
-    }, 
-    "profiles" : [{name:"build", "root":"xxx/yyy/asset/"}]
-  });
-  this.isEqual(jasy.Asset.toUri("my.png"), "xxx/yyy/asset/my.png");
-
-
-  jasy.Asset.resetData();
-  jasy.Asset.addData(
-  {
-    "assets" : {
-      "my.png" : {"p":0}
-    }, 
-    "profiles" : [{name:"build", "root":"http://mycdn.com/xxx/yyy/asset/"}]
-  });
-  this.isEqual(jasy.Asset.toUri("my.png"), "http://mycdn.com/xxx/yyy/asset/my.png");
-  
 });
-  
+

c07cc39317463dc61ddbe8a0f5caf747183f3c12
Stefan Kolb <st.kolb@telekom.de>
2014-07-01 15:26:00 +0200

    Changed the way user language is detected

--- source/class/core/detect/Locale.js
+++ source/class/core/detect/Locale.js
@@ -18,10 +18,26 @@ core.Module("core.detect.Locale",
 	 */
 	VALUE : (function(global)
 	{
-		var nav = global.navigator || {};
-		var input = (nav.userLanguage || nav.language || "en").toLowerCase();
-		var split = input.indexOf("-");
-
-		return split > 0 ? input.substring(0, split) : input;
+		
+		var DEFAULT_LANGUAGE = 'de';
+		var regexp = /([a-z]{2}-[a-z]{2})\;/ig;
+		var nav = global.navigator || { };
+		var language;
+		
+		// Let's see if we can get some language information out of the user agent
+		if (regexp.test(nav.userAgent)) {
+			language = RegExp.$1;
+		} else {
+			// Check the navigator user language and set default language if necessary
+			language = (nav.userLanguage || nav.language || DEFAULT_LANGUAGE).toLowerCase();
+		}
+		
+		if (language) {
+			var split = language.indexOf('-');
+			language = split > 0 ? language.substring(0, split) : language;
+		}
+		
+		return language || DEFAULT_LANGUAGE;
+		
 	})(core.Main.getGlobal())
 });

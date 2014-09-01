8c5eb5366640199e13d7f75ffcc038440bfd3456
Stefan Kolb <st.kolb@telekom.de>
2014-09-01 11:33:22 +0200

    Made changes so that language detection works even for incorrect language string, e.g. de- instead of de-DE, en- instead of en-US etc.

--- source/class/core/detect/Locale.js
+++ source/class/core/detect/Locale.js
@@ -18,37 +18,32 @@ core.Module("core.detect.Locale",
 	 */
 	VALUE : (function(global)
 	{
-		
-		var DEFAULT_LANGUAGE = 'de';
-		var regexp = /([a-z]{2}-[a-z]{2})\;/ig;
-		var nav = global.navigator || { };
-		var language;
-		
-		// HACK [start]
-		// We need boekhandelbe to always be in dutch
-		var qs = global.location.search;
-		if (qs.indexOf('partner=60') > -1 ||
-				qs.indexOf('partner=boekhandel') > -1 ||
-				qs.indexOf('partner=boekhandelbe') > -1 ||
-		    (global.sessionStorage && global.sessionStorage.getItem('master') === 'boekhandelbe')) {
-			return 'nl';
-		}
-		// [HACK] end
-		
-		// Let's see if we can get some language information out of the user agent
-		if (regexp.test(nav.userAgent)) {
-			language = RegExp.$1;
-		} else {
-			// Check the navigator user language and set default language if necessary
-			language = (nav.userLanguage || nav.language || DEFAULT_LANGUAGE).toLowerCase();
-		}
-		
-		if (language) {
-			var split = language.indexOf('-');
-			language = split > 0 ? language.substring(0, split) : language;
-		}
-		
-		return language || DEFAULT_LANGUAGE;
+
+    var DEFAULT_LANGUAGE = 'de';
+    var regexp = /(([a-z]{2})-([a-z]{2})?)\;?/ig;
+    var nav = global.navigator || { };
+    var language;
+
+    // HACK [start]
+    // We need boekhandelbe to always be in dutch
+    var qs = global.location.search;
+    if (qs.indexOf('partner=60') > -1 ||
+      qs.indexOf('partner=boekhandel') > -1 ||
+      qs.indexOf('partner=boekhandelbe') > -1 ||
+      (global.sessionStorage && global.sessionStorage.getItem('master') === 'boekhandelbe')) {
+      return 'nl';
+    }
+    // [HACK] end
+
+    // Let's see if we can get some language information out of the user agent
+    if (regexp.test(nav.userAgent)) {
+      language = RegExp.$2;
+    } else {
+      // Check the navigator user language and set default language if necessary
+      language = (nav.userLanguage || nav.language || DEFAULT_LANGUAGE).toLowerCase();
+    }
+
+    return language || DEFAULT_LANGUAGE;
 		
 	})(core.Main.getGlobal())
 });
